6d514d0061256f11ae4decc4c8b00c7557d0fa2a
Steve Moyer <smoyer1@selesy.com>
2025-07-04 09:40:18 -0400

    build: update Go to v1.24.4 and update build tools

--- go.mod
+++ go.mod
@@ -1,13 +1,22 @@
 module github.com/selesy/go-varsig
 
-go 1.23.3
-
-require golang.org/x/vuln v1.1.3
+go 1.24.4
 
 require (
-	golang.org/x/mod v0.19.0 // indirect
-	golang.org/x/sync v0.7.0 // indirect
-	golang.org/x/sys v0.22.0 // indirect
-	golang.org/x/telemetry v0.0.0-20240522233618-39ace7a40ae7 // indirect
-	golang.org/x/tools v0.23.0 // indirect
+	github.com/multiformats/go-multicodec v0.9.2
+	github.com/stretchr/testify v1.10.0
 )
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/mod v0.22.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/telemetry v0.0.0-20240522233618-39ace7a40ae7 // indirect
+	golang.org/x/tools v0.29.0 // indirect
+	golang.org/x/vuln v1.1.4 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+tool golang.org/x/vuln/cmd/govulncheck
--- varsig.go
+++ varsig.go
@@ -3,7 +3,7 @@
 // # Algorithm naming
 //
 // While there is no strict need for compatibility with JWA/JWT/JWE/JWS,
-// all attempts are made to keep the algorithm names here consisten with
+// all attempts are made to keep the algorithm names here consistent with
 // the table provided in [section 3.1] of RFC7518 titled "JSON Web Algorithms.
 // In cases where there is no equivalent name for an algorithm, a best-
 // effort attempt at creating a name in the spirit of that specification is
@@ -18,3 +18,106 @@
 //
 // [Varsig Specification]: https://github.com/ChainAgnostic/varsig
 package varsig
+
+import (
+	"bytes"
+	"encoding/binary"
+	"io"
+
+	"github.com/multiformats/go-multicodec"
+)
+
+// Prefix is the multicodec.Code for the varsig's varuint prefix byte.
+const Prefix = uint64(multicodec.Varsig)
+
+// Varsig represents types that describe how a signature was generated
+// and thus how to interpret the signature and verify the signed data.
+type Varsig interface {
+	// accessors for fields that are common to all varsig
+	Version() Version
+	SignatureAlgorithm() SignAlgorithm
+	PayloadEncoding() PayloadEncoding
+	Signature() []byte
+
+	// Operations that are common to all varsig
+	Encode() []byte
+}
+
+// Decode converts the provided data into one of the registered Varsig
+// types.
+func Decode(data []byte) (Varsig, error) {
+	return DefaultSignAlgorithmRegistry().Decode(bytes.NewReader(data))
+}
+
+type varsig struct {
+	vers    Version
+	signAlg SignAlgorithm
+	payEnc  PayloadEncoding
+	sig     []byte
+}
+
+// Version returns the varsig's version field.
+func (v *varsig) Version() Version {
+	return v.vers
+}
+
+// SignatureAlgorithm returns the algorithm used to produce corresponding
+// signature.
+func (v *varsig) SignatureAlgorithm() SignAlgorithm {
+	return v.signAlg
+}
+
+// PayloadEncoding returns the codec that was used to encode the signed
+// data.
+func (v *varsig) PayloadEncoding() PayloadEncoding {
+	return v.payEnc
+}
+
+// Signature returns the cryptographic signature of the signed data. This
+// value is never present in a varsig >= v1 and must either be a valid
+// signature with the correct length or empty in varsig < v1.
+func (v *varsig) Signature() []byte {
+	return v.sig
+}
+
+func (v *varsig) encode() []byte {
+	var buf []byte
+
+	buf = binary.AppendUvarint(buf, Prefix)
+
+	if v.Version() == Version1 {
+		buf = binary.AppendUvarint(buf, uint64(Version1))
+	}
+
+	buf = binary.AppendUvarint(buf, uint64(v.signAlg))
+
+	return buf
+}
+
+func (v *varsig) decodeSignature(r *bytes.Reader, varsig Varsig, expectedLength uint64) (Varsig, error) {
+	signature, err := io.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	v.sig = signature
+
+	return v.validateSignature(varsig, expectedLength)
+}
+
+func (v *varsig) validateSignature(varsig Varsig, expectedLength uint64) (Varsig, error) {
+	if v.Version() == Version0 && len(v.sig) == 0 {
+		return varsig, ErrMissingSignature
+	}
+
+	if v.Version() == Version0 && uint64(len(v.sig)) != expectedLength {
+		return nil, ErrUnexpectedSignatureSize
+	}
+
+	if v.Version() == Version1 && len(v.sig) != 0 {
+		return nil, ErrUnexpectedSignaturePresent
+	}
+
+	return varsig, nil
+
+}
