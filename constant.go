9c83def9e09df868fa9fd5ddb9b07d57aef86bce
Steve Moyer <smoyer1@selesy.com>
2025-07-07 07:36:01 -0400

    feat(constant): add types/decoders for hash algorithms and payload encoding

---
+++ constant.go
@@ -0,0 +1,119 @@
+package varsig
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+
+	"github.com/multiformats/go-multicodec"
+)
+
+// HashAlgorithm is the multicodec.Code that specifies the hash algorithm
+// that's used to reduced the signed content
+type HashAlgorithm uint64
+
+const (
+	HashAlgorithmUnspecified HashAlgorithm = 0x00
+	HashAlgorithmSHA256      HashAlgorithm = HashAlgorithm(multicodec.Sha2_256)
+	HashAlgorithmSHA384      HashAlgorithm = HashAlgorithm(multicodec.Sha2_384)
+	HashAlgorithmSHA512      HashAlgorithm = HashAlgorithm(multicodec.Sha2_512)
+)
+
+// DecodeHashAlgorithm reads and validates the expected hash algorithm
+// (for varsig types include a variable hash algorithm.)
+func DecodeHashAlgorithm(r *bytes.Reader) (HashAlgorithm, error) {
+	u, err := binary.ReadUvarint(r)
+	if err != nil {
+		return HashAlgorithmUnspecified, fmt.Errorf("%w: %w", ErrUnknownHashAlgorithm, err)
+	}
+
+	h := HashAlgorithm(u)
+
+	if _, ok := map[HashAlgorithm]struct{}{
+		HashAlgorithmUnspecified: {},
+		HashAlgorithmSHA256:      {},
+		HashAlgorithmSHA384:      {},
+		HashAlgorithmSHA512:      {},
+	}[h]; !ok {
+		return HashAlgorithmUnspecified, ErrUnknownHashAlgorithm
+	}
+
+	return h, nil
+}
+
+// PayloadEncoding specifies the encoding of the data being (hashed and)
+// signed.  A canonical representation of the data is required to produce
+// consistent hashes and signatures.
+type PayloadEncoding uint64
+
+const (
+	PayloadEncodingUnspecified PayloadEncoding = 0x00
+	PayloadEncodingVerbatim    PayloadEncoding = 0x5f
+	PayloadEncodingDAGPB                       = PayloadEncoding(multicodec.DagPb)
+	PayloadEncodingDAGCBOR                     = PayloadEncoding(multicodec.DagCbor)
+	PayloadEncodingDAGJSON                     = PayloadEncoding(multicodec.DagJson)
+	PayloadEncodingEIP191                      = PayloadEncoding(multicodec.Eip191)
+	PayloadEncodingJWT         PayloadEncoding = 0x6a77
+)
+
+// DecodePayloadEncoding reads and validates the expected canonical payload
+// encoding of the data to be signed.
+func DecodePayloadEncoding(r *bytes.Reader, vers Version) (PayloadEncoding, error) {
+	u, err := binary.ReadUvarint(r)
+	if err != nil {
+		return PayloadEncodingUnspecified, fmt.Errorf("%w: %w", ErrUnsupportedPayloadEncoding, err)
+	}
+
+	payEnc := PayloadEncoding(u)
+
+	switch vers {
+	case Version0:
+		return decodeEncodingInfoV0(payEnc)
+	case Version1:
+		return decodeEncodingInfoV1(payEnc)
+	default:
+		return 0, ErrUnsupportedVersion
+	}
+}
+
+// https://github.com/ChainAgnostic/varsig#4-payload-encoding
+func decodeEncodingInfoV0(payEnc PayloadEncoding) (PayloadEncoding, error) {
+	if _, ok := map[PayloadEncoding]struct{}{
+		PayloadEncodingVerbatim: {},
+		PayloadEncodingDAGPB:    {},
+		PayloadEncodingDAGCBOR:  {},
+		PayloadEncodingDAGJSON:  {},
+		PayloadEncodingJWT:      {},
+		PayloadEncodingEIP191:   {},
+	}[payEnc]; !ok {
+		return PayloadEncodingUnspecified, fmt.Errorf("%w: version=%d, encoding=%x", ErrUnsupportedPayloadEncoding, Version0, payEnc)
+	}
+
+	return payEnc, nil
+}
+
+// https://github.com/expede/varsig/blob/main/README.md#payload-encoding
+func decodeEncodingInfoV1(payEnc PayloadEncoding) (PayloadEncoding, error) {
+	if _, ok := map[PayloadEncoding]struct{}{
+		PayloadEncodingVerbatim: {},
+		PayloadEncodingDAGCBOR:  {},
+		PayloadEncodingDAGJSON:  {},
+		PayloadEncodingEIP191:   {},
+	}[payEnc]; !ok {
+		return PayloadEncodingUnspecified, fmt.Errorf("%w: version=%d, encoding=%x", ErrUnsupportedPayloadEncoding, Version1, payEnc)
+	}
+
+	return payEnc, nil
+}
+
+// SignAlgorithm is (usually) the multicodec.Code representing the public
+// key type of the algorithm used to create the signature.
+//
+// There is not set list of constants here, nor is there a decode function
+// as the author of an implementation should include the constant with the
+// implementation, and the decoding is handled by the Handler, which uses
+// the SignAlgorithm to choose the correct implementation.  Also note that
+// some of the SignAlgorithm values for a specific implementation have
+// changed between varsig v0 and v1, so it's possible to have more than one
+// constant defined per implementation.
+type SignAlgorithm uint64
---
+++ constant_test.go
@@ -0,0 +1,107 @@
+package varsig_test
+
+import (
+	"bytes"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/selesy/go-varsig"
+)
+
+func TestDecodeHashAlgorithm(t *testing.T) {
+	t.Parallel()
+
+	t.Run("passes", func(t *testing.T) {
+		t.Parallel()
+
+		hashAlg, err := varsig.DecodeHashAlgorithm(bytes.NewReader([]byte{0x12}))
+		require.NoError(t, err)
+		require.Equal(t, varsig.HashAlgorithmSHA256, hashAlg)
+	})
+
+	t.Run("fails - truncated varsig (no bytes)", func(t *testing.T) {
+		t.Parallel()
+
+		hashAlg, err := varsig.DecodeHashAlgorithm(bytes.NewReader([]byte{}))
+		require.ErrorIs(t, err, varsig.ErrUnknownHashAlgorithm)
+		require.ErrorIs(t, err, io.EOF)
+		require.Equal(t, varsig.HashAlgorithmUnspecified, hashAlg)
+	})
+
+	t.Run("fails - unknown hash algorithm", func(t *testing.T) {
+		t.Parallel()
+
+		hashAlg, err := varsig.DecodeHashAlgorithm(bytes.NewReader([]byte{0x42}))
+		require.ErrorIs(t, err, varsig.ErrUnknownHashAlgorithm)
+		require.Equal(t, varsig.HashAlgorithmUnspecified, hashAlg)
+	})
+}
+
+func TestDecodePayloadEncoding(t *testing.T) {
+	t.Parallel()
+
+	t.Run("passes", func(t *testing.T) {
+		t.Parallel()
+
+		t.Run("v0", func(t *testing.T) {
+			t.Parallel()
+
+			payEnc, err := varsig.DecodePayloadEncoding(bytes.NewReader([]byte{0x5f}), varsig.Version1)
+			require.NoError(t, err)
+			require.Equal(t, varsig.PayloadEncodingVerbatim, payEnc)
+		})
+
+		t.Run("v1", func(t *testing.T) {
+			t.Parallel()
+
+			payEnc, err := varsig.DecodePayloadEncoding(bytes.NewReader([]byte{0x5f}), varsig.Version1)
+			require.NoError(t, err)
+			require.Equal(t, varsig.PayloadEncodingVerbatim, payEnc)
+		})
+	})
+
+	t.Run("fails", func(t *testing.T) {
+		t.Parallel()
+
+		tests := []struct {
+			name string
+			data []byte
+			vers varsig.Version
+			err  error
+		}{
+			{
+				name: "unsupported encoding - v0",
+				data: []byte{0x42}, // random
+				vers: varsig.Version0,
+				err:  varsig.ErrUnsupportedPayloadEncoding,
+			},
+			{
+				name: "unsupported encoding - v1",
+				data: []byte{0x6a, 0x77}, // JWT
+				vers: varsig.Version1,
+				err:  varsig.ErrUnsupportedPayloadEncoding,
+			},
+			{
+				name: "unsupported version",
+				data: []byte{0x5f}, // Verbatim
+				vers: 99,           // random
+				err:  varsig.ErrUnsupportedVersion,
+			},
+		}
+
+		for _, tt := range tests {
+			tt := tt
+			t.Run(tt.name, func(t *testing.T) {
+				t.Parallel()
+
+				r := bytes.NewReader(tt.data)
+				_, err := varsig.DecodePayloadEncoding(r, tt.vers)
+				require.ErrorIs(t, err, tt.err)
+				// t.Log(err)
+				// t.Fail()
+			})
+		}
+	})
+}
