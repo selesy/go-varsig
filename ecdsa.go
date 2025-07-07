af00c4ff6c76361290136723046117ac3c85ce5e
Steve Moyer <smoyer1@selesy.com>
2025-07-07 16:29:30 -0400

    fix(ecdsa): add stub to make checks pass on Github

---
+++ ecdsa.go
@@ -0,0 +1,9 @@
+package varsig
+
+// Stub
+const (
+	SignAlgorithmECDSASecp256k1 SignAlgorithm = 0xe7
+	SignAlgorithmECDSAP256      SignAlgorithm = 0x1200
+	SignAlgorithmECDSAP384      SignAlgorithm = 0x1201
+	SignAlgorithmECDSAP521      SignAlgorithm = 0x1202
+)
